3c6a1601098333eafc6eb78581f6c28b3d5f3ff7
parzi-val <krbala1511@gmail.com>
2024-04-29 01:24:23 +0530

    user-avatars folder

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -125,13 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+MEDIA_URL = '/images/user-avatars/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images' 
+MEDIA_ROOT = BASE_DIR / 'static/images/user-avatars' 
 
 #STATIC_ROOT =
 
